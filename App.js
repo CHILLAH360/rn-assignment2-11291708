f11e1da950373489ffd70611e18f16d422bbbadc
CHILLAH360 <ewchilla@st.ug.edu.gh>
2024-05-27 05:34:17 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={{fontWeight:'bold',fontSize:24}}>My name is EMMANUEL CHILLA</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+    
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ADD8E6',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
